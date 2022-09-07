660ffa5d4c02d74462942aa087bb42b68546ed23
Blackysynch <ornelledavilla@yahoo.fr>
2022-09-06 22:14:11 -0700

    strlen

--- function.c
+++ function.c
@@ -1,2 +1,16 @@
 #include "main.h"
 
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
+ */
+
+int _strlen(const char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
