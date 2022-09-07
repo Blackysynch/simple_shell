45fed9cbcdeebe0c99f4c93403ced309b43d5e27
Blackysynch <ornelledavilla@yahoo.fr>
2022-09-07 13:46:48 -0700

    header file

--- function.c
+++ function.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * _strlen - find the length of a string
--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * execute - executes the command
