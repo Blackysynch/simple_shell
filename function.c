7244d1637199c7b4014504188142f29f7e727fda
Blackysynch <ornelledavilla@yahoo.fr>
2022-09-06 22:22:04 -0700

    strcpy

--- function.c
+++ function.c
@@ -14,3 +14,24 @@ int _strlen(const char *s)
 	return (i);
 }
 
+/**
+ * *_strcpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	int = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+
+	return (dest);
+}
+
