fc17153467c11946f23f7a8cef0fde4efc7c9fc6
Blackysynch <ornelledavilla@yahoo.fr>
2022-09-07 22:09:58 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -1,18 +1,61 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdbool.h>
 
-int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
-int _putchar(char c);
-void _puts(char *str);
-unsigned int nbr_spaces(char *s);
-char **stringToTokens(char *str);
+/* environment variables */
+extern char **environ;
+extern __sighandler_t signal(int __sig, __sighandler_t __handler);
+
+/* handle built ins */
+int checker(char **cmd, char *buf);
+void prompt_user(void);
+void handle_signal(int m);
+char **tokenizer(char *line);
+char *test_path(char **path, char *command);
+char *append_path(char *path, char *command);
+int handle_builtin(char **command, char *line);
+void exit_cmd(char **command, char *line);
+
+void print_env(void);
+
+/* string handlers */
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+char *_strdup(char *s);
+char *_strchr(char *s, char c);
+
+void execution(char *cp, char **cmd);
+char *find_path(void);
+
+/* helper function for efficient free */
+void free_buffers(char **buf);
+
+struct builtin
+{
+	char *env;
+	char *exit;
+} builtin;
+
+struct info
+{
+	int final_exit;
+	int ln_count;
+} info;
+
+struct flags
+{
+	bool interactive;
+} flags;
 
 #endif
